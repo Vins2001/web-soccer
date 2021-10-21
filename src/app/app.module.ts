b67760bc5f9546593e072b2eba164929a91debaf
Vins2001 <leniirawati01@gmail.com>
2021-10-21 12:21:34 +0700

    projek pertama git hub

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ScoringsComponent } from './scorings/scorings.component';
+import { StandingsComponent } from './standings/standings.component';
+import { AdminComponent } from './admin/admin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ScoringsComponent,
+    StandingsComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
